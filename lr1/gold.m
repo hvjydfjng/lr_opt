f4cd9c4ffb10477735df0fba9e8c753bf96076f1
Pacifist <barybin.stenpan@gmail.com>
2024-11-11 21:16:18 +0300

    С чатом исправил. Остались хорды и Ньютон

--- lr1/gold.m
+++ lr1/gold.m
@@ -1,46 +1,40 @@
-function [fmin,xmin] = gold(F,a,b,eps)
-    n = (b-a)/(eps);                  %Задаём начальное значение шага для построения графика
-    x_arr = linspace(a,b,n);
-    plot(x_arr,y(F,x_arr));
-    grid on;
-    t = (sqrt(5)-1)/2;
+function [fmin, xmin] = gold(F, a, b, eps)
+    t = (sqrt(5) - 1) / 2;
     n = 1;
-    while 1
-        if n == 1
-            [x1,x2] = x_solve(a,b);
-            f1 = y(F,x1);
-            f2 = y(F,x2);
-            curr_eps = (b-a)/2;
-        end
-        if curr_eps < eps
-           break; 
-        end
-        if f1 <= f2 && n > 1
+    
+    % Начальные значения
+    [x1, x2] = x_solve(a, b);
+    f1 = y(F, x1);
+    f2 = y(F, x2);
+    
+    while (b - a) / 2 > eps
+        if f1 <= f2
             b = x2;
             x2 = x1;
             f2 = f1;
-            x1 = b - t*(b-a);
-            f1 = y(F,x1);
-        end
-        if f1 > f2 && n > 1
+            x1 = b - t * (b - a);
+            f1 = y(F, x1);
+        else
             a = x1;
             x1 = x2;
             f1 = f2;
-            x2 = b - t*(b-a);
-            f2 = y(F,x2);
+            x2 = a + t * (b - a);
+            f2 = y(F, x2);
         end
-        n = n+1;
-        curr_eps = curr_eps * t;
+        n = n + 1;
     end
-    xmin = double((a+b)/2);
-    fmin = double(y(F,xmin));
-end    
     
-    function [fx] = y(F,xi)
-        fx = subs(F,xi);
-    end
+    xmin = (a + b) / 2;
+    fmin = y(F, xmin);
+end    
 
-    function [x1,x2] = x_solve(a,b)
-        x1 = a+(3-sqrt(5))/2*(b-a);
-        x2 = a+(sqrt(5)-1)/2*(b-a);
-    end
+function fx = y(F, xi)
+    fx = F(xi);  % Прямое вычисление для числовой функции
+end
+
+function [x1, x2] = x_solve(a, b)
+    t1 = (3 - sqrt(5)) / 2;
+    t2 = (sqrt(5) - 1) / 2;
+    x1 = a + t1 * (b - a);
+    x2 = a + t2 * (b - a);
+end
--- lr1/parabols.m
+++ lr1/parabols.m
@@ -1,12 +1,12 @@
-function [fmin,xmin] = parabols(F,a,b,eps)
-    n = (b-a)/(eps);                  %Задаём начальное значение шага для построения графика
-    x_arr = linspace(a,b,n);
-    plot(x_arr,y(F,x_arr));
-    grid on;
+function [fmin, xmin] = parabols(F, a, b, eps)
+    % Начальные точки методом золотого сечения
     t = (sqrt(5)-1)/2;
-    n = 1;
-    a0 = a;                           %Для проверки условия с концами отрезков
+    [x1, x2] = x_solve(a, b);
+    x3 = b;
+    
+     a0 = a;                           %Для проверки условия с концами отрезков
     b0 = b;
+    n = 1;
     while 1                           %Несколько итераций золотого сечения для поиска подходящих точек
         if n == 1
             [x1,x2] = x_solve(a,b);
@@ -44,69 +44,63 @@ function [fmin,xmin] = parabols(F,a,b,eps)
         curr_eps = curr_eps * t;
     end
     n = 1;
-    %Получены начальные точки для метода парабол.
-    x_ = y(F,x2);                    %Начальное значение для мнмимума
-    x1_new = x1;
-    x2_new = x2;
-    x3_new = x3;
+    
+    % Значения функции в точках
+    f1 = y(F, x1);
+    f2 = y(F, x2);
+    f3 = y(F, x3);
+
+    % Итерационный процесс
     while 1
-        if n == 1
-           f1 = y(F,x1);
-           f2 = y(F,x2);
-           f3 = y(F,x3);
-        end
-        a0 = f1;
-        a1 = (f2-f1)/(x2-x1);
-        a2 = 1/(x3-x2)*( (f3-f1)/(x3-x1) - (f2-f1)/(x2-x1));
-        %Если х поменяли значение вычисляем новое f(x) и меняем значение х
-        %Не знаю как но надо добавить проверку на равенство иксов
-        if(x1 ~= x1_new)
-            f1 = y(F,x1_new);
-        end
-        if(x2 ~= x2_new)
-            f2 = y(F,x2_new);
-        end
-        if(x3 ~= x3_new)
-            f3 = y(F,x3_new);
-        end
-        x_old = x_;
-        x1 = x1_new;
-        x2 = x2_new;
-        x3 = x3_new;
-        x_ = 1/2*(x1+x2-a1/a2);
-        %Рассматриваем варианты точки минимума
-        if x_ >= x1 && x_ < x2
-            if y(F,x_) >= f2
-                x1_new = x_;
+        % Вычисление коэффициентов параболы
+        a1 = (f2 - f1) / (x2 - x1);
+        a2 = 1 / (x3 - x2) * ((f3 - f1) / (x3 - x1) - (f2 - f1) / (x2 - x1));
+        
+        % Новая точка минимума
+        x_ = 0.5 * (x1 + x2 - a1 / a2);
+        f_ = y(F, x_);
+        
+        % Обновление точек в зависимости от положения минимума
+        if x_ > x1 && x_ < x2
+            if f_ < f2
+                x3 = x2;
+                f3 = f2;
+                x2 = x_;
+                f2 = f_;
             else
-                x2_new = x_;
-                x3_new = x_;
+                x1 = x_;
+                f1 = f_;
+            end
+        elseif x_ > x2 && x_ < x3
+            if f_ < f2
+                x1 = x2;
+                f1 = f2;
+                x2 = x_;
+                f2 = f_;
+            else
+                x3 = x_;
+                f3 = f_;
             end
         end
-        if x_ <= x3 && x_ > x2
-            if y(F,x_) >= f2
-                x3_new = x_;
-            else
-                x1_new = x_;
-                x2_new = x_;
-            end        
-        end
-%        if x_ == x2
-%            x2_new = x_;
-%        end
-        if abs(x_old - x_) < eps
+
+        % Проверка критерия остановки
+        if abs(x_ - x2) < eps
             break;
         end
-    n = n + 1;    
-    end
-    xmin = double(x_);
-    fmin = double(y(F,xmin));
-end
-    function [fx] = y(F,xi)
-        fx = subs(F,xi);
     end
 
-    function [x1,x2] = x_solve(a,b)
-        x1 = a+(3-sqrt(5))/2*(b-a);
-        x2 = a+(sqrt(5)-1)/2*(b-a);
-    end
+    xmin = double(x_);
+    fmin = double(f_);
+end
+
+% Вспомогательная функция для вычисления значений функции
+function [fx] = y(F, xi)
+    fx = double(subs(F, xi));
+end
+
+% Вспомогательная функция для вычисления точек x1 и x2 методом золотого сечения
+function [x1, x2] = x_solve(a, b)
+    t = (sqrt(5) - 1) / 2;
+    x1 = a + (3 - sqrt(5)) / 2 * (b - a);
+    x2 = a + t * (b - a);
+end
